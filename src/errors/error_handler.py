4d6a26778755d14df1c64acda50b92cbe2855e3e
Martone Santana <martonesantana@PCCasa>
2024-02-10 19:38:07 -0300

    feat: implement validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,5 +1,12 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={"errors": [{"title": error.name, "detail": error.message}]},
+        )
+
     return HttpResponse(status_code=400, body={"errors": [{"message": str(error)}]})
---
+++ src/validators/tag_creator_validator.py
@@ -0,0 +1,14 @@
+from cerberus import Validator
+from src.errors.error_types.http_unprocessable_entity import (
+    HttpUnprocessableEntityError,
+)
+
+
+def tag_creator_validator(request: any) -> None:
+    body_validation = Validator(
+        {"product_code": {"type": "string", "required": True, "empty": False}}
+    )
+
+    response = body_validation.validate(request.json)
+    if response is False:
+        raise HttpUnprocessableEntityError(body_validation.errors)
